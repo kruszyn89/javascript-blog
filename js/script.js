3c978cd7cac7816ece4c83a55f67bccde6a97fd4
Dawid Kruszyński <dawid.kruszynski89@gmail.com>
2021-10-18 22:29:37 +0200

    active removed from titles and articles

--- js/script.js
+++ js/script.js
@@ -1,30 +1,50 @@
 'use strict'
 
-const titleClickHandler = document.addEventListener('click', function(){
+const titleClickHandler = document.addEventListener('click', function(event){
     console.log('Link was clicked!', event);
-});
-
-    /* remove class 'active' from all article links  */
-  
-    /* add class 'active' to the clicked link */
-  
-    /* remove class 'active' from all articles */
-  
-    /* get 'href' attribute from the clicked link */
-  
-    /* find the correct article using the selector (value of 'href' attribute) */
-  
-    /* add class 'active' to the correct article */
 
 
-  
+/* remove class 'active' from all article links  */
+
+const activeLinks = document.querySelectorAll('.titles a.active');
+
+for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+}
+
+/* add class 'active' to the clicked link */
+
+
+
+/* remove class 'active' from all articles */
+
+const activeArticles = document.querySelectorAll('.posts .active');
+
+for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+} 
+
+
+/* get 'href' attribute from the clicked link */
+
+
+
+/* find the correct article using the selector (value of 'href' attribute) */
+
+
+
+/* add class 'active' to the correct article */
+
+
+
+
 const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
     link.addEventListener('click', titleClickHandler);
 }
 
-
+});
 
 
 
