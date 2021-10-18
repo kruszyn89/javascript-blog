16b4b14a867998a1b2bbb882727e51722e40b854
Dawid Kruszyński <dawid.kruszynski89@gmail.com>
2021-10-18 16:06:56 +0200

    event console.log

---
+++ js/script.js
@@ -0,0 +1,52 @@
+'use strict'
+
+const titleClickHandler = document.addEventListener('click', function(){
+    console.log('Link was clicked!', event);
+});
+
+    /* remove class 'active' from all article links  */
+  
+    /* add class 'active' to the clicked link */
+  
+    /* remove class 'active' from all articles */
+  
+    /* get 'href' attribute from the clicked link */
+  
+    /* find the correct article using the selector (value of 'href' attribute) */
+  
+    /* add class 'active' to the correct article */
+
+
+  
+const links = document.querySelectorAll('.titles a');
+
+for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.getElementById('test-button').addEventListener('click', function(){
+//     const links = document.querySelectorAll('.titles a');
+//     console.log('links:', links);
+//   });
