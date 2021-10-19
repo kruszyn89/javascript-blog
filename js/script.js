ac33587c008957f2c87d03863896cc70deb7173d
Dawid Kruszyński <dawid.kruszynski89@gmail.com>
2021-10-19 14:13:11 +0200

    6.3 correction v2

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
-    'use strict';
+{'use strict';
     
-    const titleClickHandler = function(event) {
+    /* Article reflection after clicking */
+    const titleClickHandler = function (event) {   
         event.preventDefault();
         const clickedElement = this;
-        console.log('Link was clicked!');
-    
+        console.log('Link was clicked!', event);
+  
         /* [DONE] remove class 'active' from all article links  */
         
         const activeLinks = document.querySelectorAll('.titles a.active');
 
         for(let activeLink of activeLinks) {
-            activeLink.classList.remove('active');
+            activeLink.classList.remove("active");
         }
 
         /* [DONE yet error]  add class 'active' to the clicked link */
@@ -40,7 +41,45 @@
         targetArticle.classList.add('active');
         console.log('targetArticle', targetArticle);
 
-    };
+        return titleClickHandler();
+
+        };
+
+        
+
+   /* Generate Title List */
+    const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
+
+    function generateTitleLinks() {
+        console.log('title list generated: ');
+    
+        /* remove contents of titleList */
+        const titleList = document.querySelector(optTitleListSelector);
+        titleList.innerHTML = '';
+        
+        /* for each article */
+
+        /* get the article id */
+
+        /* find the title element */
+        
+        /* get the title from the title element */
+
+        /* create HTML of the link */
+
+        /* insert link into titleList */
+
+        
+    }   
+    
+}      
+
+ 
+
+
+
 
 
 
